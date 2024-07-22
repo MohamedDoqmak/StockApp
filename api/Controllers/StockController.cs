1f0e8d1a16397b2259fed8d426e90a1c0ca6a5c5
mohamedAyman <modyayman2001@gmail.com>
2024-07-23 00:00:08 +0300

    Added a Dto and Mapper for Stock

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -16,12 +17,15 @@ namespace api.Controllers
         {
             _context = context;
         }
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList()
+            .Select(s => s.ToStockDto());
             return Ok(stocks);
         }
+
         [HttpGet("{id}")]
         public IActionResult GetById([FromRoute] int id)
         {
@@ -30,7 +34,7 @@ namespace api.Controllers
             {
                 return NotFound();
             }
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
     }
 }
