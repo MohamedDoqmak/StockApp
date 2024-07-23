e825f2b86eb40d349734f176a200187c30c585f0
mohamedAyman <modyayman2001@gmail.com>
2024-07-23 21:06:57 +0300

    Hotfix column name typo

---
+++ api/Migrations/20240723180417_RenamedColumnpurchaseToPurchaseInStock.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedColumnpurchaseToPurchaseInStock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "purchase",
+                table: "Stocks",
+                newName: "Purchase");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Purchase",
+                table: "Stocks",
+                newName: "purchase");
+        }
+    }
+}
--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -12,7 +12,7 @@ namespace api.Models
         public string Symbol { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
         [Column(TypeName = "decimal(18,2)")]
-        public decimal purchase { get; set; }
+        public decimal Purchase { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal LastDiv { get; set; }
         public string Industry { get; set; } = string.Empty;
